8e71f97d7b53e81370832d060609c05735f2e1c8
odemyanenko <olgadem@hotmail.com>
2023-05-30 14:17:03 +0300

    Add files via upload

---
+++ tasks/scripts_secunde_minutes.sql
@@ -0,0 +1,57 @@
+-- secunde
+create table seconds as 
+select 1 as second
+union all
+select 2 as second
+union all
+select 3 as second
+union all
+select 4 as second
+union all
+select 5 as second
+union all
+select 6 as second
+union all
+select 7 as second
+union all
+select 8 as second
+union all
+select 9 as second
+union all
+select 10 as second
+union all
+select 5 as second
+union all
+select 6 as second
+union all
+select 7 as second
+union all
+select 8 as second
+union all
+select 9 as second
+union all
+select 10 as second
+;
+-- minutes 
+
+create table minutes as 
+select 1 as minute
+union all
+select 2 as second
+union all
+select 3 as second
+union all
+select 4 as second
+union all
+select 5 as second
+union all
+select 6 as second
+union all
+select 7 as second
+union all
+select 8 as second
+union all
+select 9 as second
+union all
+select 10 as second
+;
