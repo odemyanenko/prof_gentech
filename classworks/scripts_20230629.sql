e91bc97e1ecc9e905cd269bd04caacc36b3a6161
odemyanenko <olgadem@hotmail.com>
2023-07-04 00:07:29 +0200

    Add files via upload

---
+++ classworks/scripts_20230629.sql
@@ -0,0 +1,13 @@
+-- Telegram: @NickKorovin
+-- https://raw.githubusercontent.com/NelliEfr/hr_data/master/script.sql
+
+-- MongoDB
+-- https://www.mongodb.com/try/download/community
+-- Windows install .msi
+-- !!! check MongoDB Compass
+
+-- back end swagger for documentation--------------------
+
+
+
+
