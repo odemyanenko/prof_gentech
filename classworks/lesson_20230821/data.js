470469f2f7403cb8acef53090c467e945bce3c5b
odemyanenko <olgadem@hotmail.com>
2023-08-28 10:06:09 +0300

    Add files via upload

---
+++ classworks/lesson_20230821/data.js
@@ -0,0 +1,66 @@
+db.students.insertMany([
+  {
+    "first_name": "Анна",
+    "last_name": "Иванова",
+    "age": 20,
+    "gender": "f",
+    "ciry": "Berlin",
+    "marks": [5, 4, 6, 7, 3]
+  },
+  {
+    "first_name": "Петр",
+    "last_name": "Смирнов",
+    "age": 22,
+    "gender": "m",
+    "ciry": "Bremen",
+    "marks": [6, 6, 5, 7, 4]
+  },
+  {
+    "first_name": "Елена",
+    "last_name": "Морозова",
+    "age": 21,
+    "gender": "f",
+    "ciry": "New-York",
+    "marks": [7, 7, 6, 6, 7]
+  },
+  {
+    "first_name": "Иван",
+    "last_name": "Козлов",
+    "age": 23,
+    "gender": "m",
+    "ciry": "Berlin",
+    "marks": [4, 5, 3, 4, 6]
+  },
+  {
+    "first_name": "Мария",
+    "last_name": "Петрова",
+    "age": 20,
+    "gender": "f",
+    "ciry": "Bremen",
+    "marks": [7, 6, 7, 7, 5]
+  },
+  {
+    "first_name": "Алексей",
+    "last_name": "Соколов",
+    "age": 22,
+    "gender": "m",
+    "ciry": "New-York",
+    "marks": [3, 4, 3, 5, 2]
+  },
+  {
+    "first_name": "Анна",
+    "last_name": "Михайлова",
+    "age": 21,
+    "gender": "f",
+    "ciry": "Berlin",
+    "marks": [6, 7, 5, 6, 7]
+  },
+  {
+    "first_name": "Дмитрий",
+    "last_name": "Волков",
+    "age": 23,
+    "gender": "m",
+    "ciry": "Bremen",
+    "marks": [4, 5, 4, 3, 6]
+  }
+]);
